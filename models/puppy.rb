744918d920c53dbaa529d3fde4dd82f26afc66a0
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 10:21:40 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,9 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+  attr_accessor :name, :breed
+
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+end
