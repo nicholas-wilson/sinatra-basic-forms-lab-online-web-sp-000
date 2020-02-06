c959479fe1f1aaf845d2f017f7ba349aadd3e566
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 10:26:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/puppy' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    erb :display_puppy
+  end
 end
